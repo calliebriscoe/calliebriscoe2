2b0390f5353495a08973734731468c556b7cde7c
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-14 22:51:55 -0500

    added foreign key for users in the posts

--- database/migrations/2016_03_09_044917_create_posts_table.php
+++ database/migrations/2016_03_09_044917_create_posts_table.php
@@ -14,6 +14,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integar('users_id')->unsigned();
             $table->string('title');
             $table->text('description');
             $table->timestamps();
