aa089a95722b2babdc75dac23397e71cb81acf39
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-15 16:27:30 -0500

    added porjects migration

---
+++ database/migrations/2016_03_15_211557_create_projects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integar('users_id')->unsigned();
+            $table->integar('experiences_id')->unsigned();
+            $table->string('title');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('project_role');
+            $table->string('project_img');
+            $table->string('description');
+            $table->string('github_link');
+            $table->string('web_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
