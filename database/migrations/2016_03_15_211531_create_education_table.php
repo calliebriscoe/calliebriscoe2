1170c66f6c61e2227a70f51917023f979ee050e2
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-15 16:26:09 -0500

    added education migration

---
+++ database/migrations/2016_03_15_211531_create_education_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEducationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integar('users_id')->unsigned();
+            $table->string('main_image_edu');
+            $table->string('school_name');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('main_description');
+            $table->string('skill_head_1');
+            $table->string('skill_img_1');
+            $table->string('skill_description_1');
+            $table->string('skill_head_2');
+            $table->string('skill_img_2');
+            $table->string('skill_description_2');
+            $table->string('skill_head_3');
+            $table->string('skill_img_3');
+            $table->string('skill_description_3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('education');
+    }
+}
