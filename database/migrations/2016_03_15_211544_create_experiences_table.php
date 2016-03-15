c1deeaba465a4c47c88b5fe1df00fd46b202e9ac
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-15 16:26:50 -0500

    added expirence migration

---
+++ database/migrations/2016_03_15_211544_create_experiences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integar('users_id')->unsigned();
+            $table->string('exp_image');
+            $table->string('job_title');
+            $table->string('company');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('experiences');
+    }
+}
