e795a545c6a9a44b8aa56cf29071dd045d70191f
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-15 22:02:50 -0500

    updated Post model to include relationships

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  /**
+  	 * The database table used by the model.
+  	 *
+  	 * @var string
+  	 */
+  	protected $table = 'posts';
+  	/**
+  	 * Get the user that owns the post.
+  	 *
+  	 * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+  	 */
+  	public function user()
+  	{
+  		return $this->belongsTo('App\User');
+  	}
+  	/**
+  	 * Get the comments of the post.
+  	 *
+  	 * @return Illuminate\Database\Eloquent\Relations\hasMany
+  	 */
+  	public function comments()
+  	{
+  		return $this->hasMany('App\Comment');
+  	}
 }
