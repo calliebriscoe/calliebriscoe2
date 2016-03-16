ef640e7dada122ad12a42c0d88cfa8fd1fc9e205
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-15 22:02:26 -0500

    updated Comment model to include relationships

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'comments';
+  /**
+   * Get the user that owns the comment.
+   */
+  public function user()
+  {
+      return $this->belongsTo('App\User');
+  }
+   /**
+   * Get the post that owns the comment.
+   */
+  public function post()
+  {
+      return $this->belongsTo('App\Post');
+  }
+   /**
+   * Get the parent comment that owns the comment.
+   */
+  public function parentComment()
+  {
+      return $this->belongsTo('App\Comment');
+  }
+   /**
+   * Get the child comments owned by the comment.
+   */
+  public function childComments()
+  {
+      return $this->hasMany('App\Comment');
+  }
 }
