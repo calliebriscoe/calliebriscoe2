37e626765d4d36fc8103224da025dae14aab3971
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-16 20:46:01 -0500

    event refactoring

--- app/Events/Event.php
+++ app/Events/Event.php
@@ -1,8 +1,18 @@
 <?php
-
 namespace App\Events;
-
-abstract class Event
+use App\Events\Event;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+class UserAccess extends Event
 {
-    //
+    use SerializesModels;
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
 }
