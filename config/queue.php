53ab2b1204fe70e31e8d86527e9d0cb6f0a2c840
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-23 23:14:45 -0500

    modified queue.php

--- config/queue.php
+++ config/queue.php
@@ -58,6 +58,15 @@ return [
             'region' => 'us-east-1',
         ],
 
+    		'iron' => [
+    			'driver'  => 'iron',
+    			'host'    => 'mq-aws-us-east-1.iron.io',
+    			'token'   => 'your-token',
+    			'project' => 'your-project-id',
+    			'queue'   => 'your-queue-name',
+    			'encrypt' => true,
+    		],
+
         'redis' => [
             'driver'     => 'redis',
             'connection' => 'default',
