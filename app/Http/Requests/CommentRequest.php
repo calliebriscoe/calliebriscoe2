1f77aa7ea11b8d460fb77fa692bb291aa3367579
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-26 22:00:36 -0500

    added request for Comments

---
+++ app/Http/Requests/CommentRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+class CommentRequest extends Request
+{
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		$id = $this->comment;
+		return [
+			'comments' . $id => 'required|max:65000',
+		];
+	}
+}
